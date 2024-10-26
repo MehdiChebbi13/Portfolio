6640e6faf894694a3b12ff9f832a5250414aa498
Mahdi Chebbi <158098125+MehdiChebbi13@users.noreply.github.com>
2024-10-26 22:46:45 +0100

    added techStack

---
+++ components/Grid.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Grid() {
+  return (
+    <section id="projects">
+      <div className="flex flex-col mx-auto ">
+        <p className="text-center text-[#ffffff] font-medium md:tracking-wider my-4 text-[30px] md:text-3xl lg:text-4xl">
+          Technologies que j’utilise
+        </p>
+        <div className="flex flex-wrap gap-2 border-2 rounded-lg">
+          <p>HTML</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Grid;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import MagicButton from "./ui/MagicButton";
 
 function Hero() {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-52 pt-36">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
