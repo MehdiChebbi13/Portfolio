eaea85442612280e3f9dfa4d59ecb45167519fd8
Mahdi Chebbi <158098125+MehdiChebbi13@users.noreply.github.com>
2024-10-29 18:36:07 +0100

    added project card

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,66 +1,227 @@
-import { projects } from "@/data";
-import React from "react";
+"use client";
+import { cards } from "@/data";
+import Image from "next/image";
+import React, { useEffect, useId, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useOutsideClick } from "@/hooks/use-outside-click";
 import { PinContainer } from "./ui/PinContainer";
 
 function Projects() {
+  const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(
+    null
+  );
+  const ref = useRef<HTMLDivElement>(null);
+  const id = useId();
+
+  useEffect(() => {
+    function onKeyDown(event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        setActive(false);
+      }
+    }
+
+    if (active && typeof active === "object") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [active]);
+
+  useOutsideClick(ref, () => setActive(null));
+
   return (
-    <div className="py-20 max-w-6xl mx-auto " id="project">
-      <h1 className="heading">
-        Une petite selection de{" "}
-        <span className="text-purple">projets recents</span>
-      </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
-        {projects.map((item) => (
-          <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-            key={item.id}
-          >
-            <PinContainer>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
-                >
-                  <img src="/bg.png" alt="bgimg" />
-                </div>
-                <img
-                  src={item.img}
-                  alt="cover"
-                  className="z-10 absolute bottom-0"
+    <>
+      {/* This AnimatePresence component is responsible for animating the backdrop when a card is expanded */}
+      <AnimatePresence>
+        {active && typeof active === "object" && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 bg-black/20 h-full w-full z-10"
+          />
+        )}
+      </AnimatePresence>
+
+      {/* This AnimatePresence component is responsible for animating the expanded card */}
+      <AnimatePresence>
+        {active && typeof active === "object" ? (
+          <div className="fixed inset-0  grid place-items-center z-[100]">
+            {/* This button is responsible for closing the active card */}
+            <motion.button
+              key={`button-${active.title}-${id}`}
+              layout
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              exit={{
+                opacity: 0,
+                transition: {
+                  duration: 0.05,
+                },
+              }}
+              className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
+              onClick={() => setActive(null)}
+            >
+              <CloseIcon />
+            </motion.button>
+
+            {/* This div contains the expanded card */}
+            <motion.div
+              layoutId={`card-${active.title}-${id}`}
+              ref={ref}
+              className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+            >
+              <motion.div layoutId={`image-${active.title}-${id}`}>
+                <Image
+                  priority
+                  width={200}
+                  height={200}
+                  src="/bg.png"
+                  alt={active.title}
+                  className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
                 />
-              </div>
+              </motion.div>
 
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {item.title}
-              </h1>
-
-              <div className="flex items-center justify-between mt-7 mb-3">
-                <div className="flex items-center">
-                  {item.iconLists.map((icon, index) => (
-                    <div
-                      key={index}
-                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{
-                        transform: `translateX(-${5 * index + 2}px)`,
-                      }}
+              <div>
+                <div className="flex justify-between items-start p-4">
+                  <div className="">
+                    <motion.h3
+                      layoutId={`title-${active.title}-${id}`}
+                      className="font-bold text-neutral-700 dark:text-neutral-200"
                     >
-                      <img src={icon} alt="icon5" className="p-2" />
-                    </div>
-                  ))}
+                      {active.title}
+                    </motion.h3>
+                    <motion.p
+                      layoutId={`description-${active.description}-${id}`}
+                      className="text-neutral-600 dark:text-neutral-400"
+                    >
+                      {active.description}
+                    </motion.p>
+                  </div>
                 </div>
-
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    En Savoir Plus
-                  </p>
+                <div className="pt-4 relative px-4">
+                  <motion.div
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+                  >
+                    {typeof active.content === "function"
+                      ? active.content()
+                      : active.content}
+                  </motion.div>
                 </div>
               </div>
-            </PinContainer>
+            </motion.div>
           </div>
-        ))}
+        ) : null}
+      </AnimatePresence>
+
+      {/* This is the main container for the project cards */}
+      <div className="py-20 max-w-6xl mx-auto " id="project">
+        <h1 className="heading">
+          Une petite selection de{" "}
+          <span className="text-purple">projets recents</span>
+        </h1>
+        <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+          {cards.map((item) => (
+            <motion.div
+              layoutId={`card-${item.title}-${id}`}
+              key={`card-${item.title}-${id}`}
+              onClick={() => setActive(item)}
+              className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            >
+              <PinContainer>
+                <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                  <div
+                    className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                    style={{ backgroundColor: "#13162D" }}
+                  >
+                    <img src="/bg.png" alt="bgimg" />
+                  </div>
+                  <img
+                    src={item.src}
+                    alt="cover"
+                    className="z-10 absolute bottom-0"
+                  />
+                </div>
+
+                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                  {item.title}
+                </h1>
+
+                <div className="flex items-center justify-between mt-7 mb-3">
+                  <div className="flex items-center">
+                    {item.iconLists.map((icon, index) => (
+                      <div
+                        key={index}
+                        className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                        style={{
+                          transform: `translateX(-${5 * index + 2}px)`,
+                        }}
+                      >
+                        <img src={icon} alt="icon5" className="p-2" />
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="flex justify-center items-center">
+                    <motion.button
+                      layoutId={`button-${item.title}-${id}`}
+                      className="flex lg:text-xl md:text-xs text-sm text-purple"
+                    >
+                      En Savoir Plus
+                    </motion.button>
+                  </div>
+                </div>
+              </PinContainer>
+            </motion.div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
 export default Projects;
+// The CloseIcon component is a simple SVG icon used to close the expanded card
+export const CloseIcon = () => {
+  return (
+    <motion.svg
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      exit={{
+        opacity: 0,
+        transition: {
+          duration: 0.05,
+        },
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="h-4 w-4 text-black"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M18 6l-12 12" />
+      <path d="M6 6l12 12" />
+    </motion.svg>
+  );
+};
---
+++ data/index.tsx
@@ -0,0 +1,119 @@
+export const navItems = [
+  { name: "Acceuil", link: "#about" },
+  { name: "Projets", link: "#project" },
+  { name: "Contact", link: "#contact" },
+];
+export const cards = [
+  {
+    id: 1,
+    title: "Dépôt de chèques en ligne",
+    description: "Explore the wonders ",
+    src: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg", "/docker.svg"],
+    content: () => {
+      return (
+        <p>
+          • D&eacute;veloppement d&apos;une application mobile innovante
+          appel&eacute;e D&eacute;p&ocirc;t de ch&egrave;ques en ligne lors de
+          mon stage, utilisant des technologies d&apos;intelligence artificielle
+          pour am&eacute;liorer la rapidit&eacute;, la pr&eacute;cision et
+          l&apos;accessibilit&eacute; du processus de d&eacute;p&ocirc;t de
+          ch&egrave;ques. <br />
+          • R&eacute;alisation de la conception UML, de la cr&eacute;ation des
+          maquettes avec Figma, et du d&eacute;veloppement front-end de
+          l&apos;application en utilisant TypeScript et les biblioth&egrave;ques
+          React. <br />
+          • Dockerisation du projet pour assurer une gestion efficace des
+          d&eacute;ploiements, garantissant un environnement stable et
+          reproductible.
+          <br />• Conformit&eacute; rigoureuse au cahier des charges fourni,
+          apprenant &agrave; g&eacute;rer et organiser mes t&acirc;ches de
+          mani&egrave;re structur&eacute;e dans un environnement de travail
+          professionnel. <br />• Ma&icirc;trise de TypeScript et React,
+          d&eacute;veloppement de comp&eacute;tences en gestion de projet et en
+          d&eacute;ploiement d&apos;applications conteneuris&eacute;es.
+        </p>
+      );
+    },
+  },
+  {
+    id: 2,
+    title: "Remotini Application de Télétravail",
+    description: "Simplify your video ",
+    src: "/p2.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/stream.svg"],
+    content: () => {
+      return (
+        <p>
+          • Conception et d&eacute;veloppement d&apos;une application de
+          t&eacute;l&eacute;travail nomm&eacute;e Remotini, permettant une
+          collaboration vid&eacute;o simplifi&eacute;e et fluide pour les
+          &eacute;quipes distantes. <br />
+          • Utilisation de TypeScript et React pour cr&eacute;er une interface
+          utilisateur intuitive et r&eacute;active, offrant une
+          exp&eacute;rience utilisateur am&eacute;lior&eacute;e. <br />
+          • Int&eacute;gration de biblioth&egrave;ques de streaming pour des
+          appels vid&eacute;o en temps r&eacute;el, assurant une communication
+          stable et de haute qualit&eacute; entre les utilisateurs. <br />
+          • Mise en œuvre de Tailwind CSS pour la cr&eacute;ation d&apos;une
+          interface moderne et adaptable aux diff&eacute;rents appareils. <br />
+          • Gestion des &eacute;tats et de la synchronisation en temps
+          r&eacute;el pour faciliter le travail collaboratif et productif.
+        </p>
+      );
+    },
+  },
+  {
+    id: 3,
+    title: "Responsive GYM Website",
+    description: "A REAL Software",
+    src: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    content: () => {
+      return (
+        <p>
+          • Cr&eacute;ation d&apos;un site web de salle de sport r&eacute;actif,
+          con&ccedil;u pour offrir une exp&eacute;rience utilisateur immersive
+          et accessible sur tous les appareils. <br />
+          • Utilisation de Figma pour concevoir des maquettes attrayantes et
+          harmonieuses, mettant en valeur les services de la salle de sport et
+          permettant une navigation intuitive. <br />
+          • D&eacute;veloppement front-end avec React et TypeScript, assurant
+          une interface fluide et performante. <br />
+          • Application de Tailwind CSS pour une mise en page moderne,
+          facilitant l&apos;adaptabilit&eacute; et la r&eacute;activit&eacute;
+          sur les mobiles, tablettes et ordinateurs de bureau. <br />•
+          Am&eacute;lioration de l&apos;engagement des utilisateurs gr&acirc;ce
+          &agrave; une optimisation UX/UI cibl&eacute;e.
+        </p>
+      );
+    },
+  },
+  {
+    id: 4,
+    title: "8 Puzzle Solving Algorithm",
+    description: "Recreated the Apple",
+    src: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    content: () => {
+      return (
+        <p>
+          • D&eacute;veloppement d&apos;un algorithme de r&eacute;solution du
+          jeu de puzzle 8, en utilisant diverses techniques de recherche,
+          notamment A* et le hill climbing. <br />
+          • Cr&eacute;ation de l&apos;interface en utilisant Next.js et
+          TypeScript pour une exp&eacute;rience utilisateur engageante et
+          interactive. <br />
+          • Impl&eacute;mentation de la biblioth&egrave;que Three.js pour
+          ajouter des &eacute;l&eacute;ments 3D, enrichissant
+          l&apos;exp&eacute;rience visuelle du jeu. <br />
+          • Utilisation de GSAP pour les animations, rendant le jeu plus
+          dynamique et captivant. <br />• Conception orient&eacute;e
+          performance, avec une optimisation des algorithmes pour des temps de
+          calcul rapides, assurant une r&eacute;solution fluide des puzzles pour
+          les utilisateurs.
+        </p>
+      );
+    },
+  },
+];
